54a8f8ee7507323d9448391a4ef869de63141fa8
Hojun Yoon <leid07@aispera.com>
2025-06-13 07:31:04 +0900

    응답 객체 (~Response) 를 도메인 객체로 통일, 인기 키워드 API 요청 포맷 정의
    응답 객체 (~Response) 를 도메인 객체로 통일, 인기 키워드 API 요청 포맷 정의
    
    Changelog: changed

---
+++ src/main/java/kr/hhplus/be/server/dto/PopularKeywordRequest.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.dto;
+
+public class PopularKeywordRequest {
+    private String region;
+
+    public PopularKeywordRequest() {}
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
