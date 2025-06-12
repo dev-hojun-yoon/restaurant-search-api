5b1ed699bf26c828f407f4498e882306b7b3f2b8
Hojun Yoon <leid07@aispera.com>
2025-06-13 07:35:47 +0900

    맛집 조회 부분은 레이어드 아키텍처 유지, 검색 기록 저장 기능 추가
    맛집 조회 부분은 레이어드 아키텍처 유지, 검색 기록 저장 기능 추가
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/service/RestaurantService.java
+++ src/main/java/kr/hhplus/be/server/service/RestaurantService.java
@@ -1,9 +1,10 @@
 package kr.hhplus.be.server.service;
 
-import kr.hhplus.be.server.dto.RestaurantResponse;
+import kr.hhplus.be.server.domain.restaurant.Restaurant;
+import kr.hhplus.be.server.domain.restaurant.RestaurantRepository;
 import kr.hhplus.be.server.dto.RestaurantSearchRequest;
-import kr.hhplus.be.server.external.NaverApiClient;
-import lombok.RequiredArgsConstructor;
+import kr.hhplus.be.server.infrastructure.external.NaverApiClient;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,12 +13,18 @@ import java.util.List;
 public class RestaurantService {
 
     private final NaverApiClient naverApiClient;
+    private final RestaurantRepository repository;
 
-    public RestaurantService(NaverApiClient naverApiClient) {
+    public RestaurantService(NaverApiClient naverApiClient, RestaurantRepository repository) {
         this.naverApiClient = naverApiClient;
+        this.repository = repository;
     }
 
-    public List<RestaurantResponse> searchRestaurants(RestaurantSearchRequest request) {
+    public List<Restaurant> searchRestaurants(RestaurantSearchRequest request) {
+        List<Restaurant> externalApiResponse = naverApiClient.search(request);
+        // 검색 기록 저장
+        repository.save(externalApiResponse);
+
         return naverApiClient.search(request);
     }
 }
