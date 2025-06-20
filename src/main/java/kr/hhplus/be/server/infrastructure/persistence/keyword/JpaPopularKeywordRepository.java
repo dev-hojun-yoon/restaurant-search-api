b4daff712fe5843c4cf6910858d683923c90d578
Hojun Yoon <leid07@aispera.com>
2025-06-20 23:10:18 +0900

    조건부 Update 방식으로 동시성 제어
    조건부 Update 방식으로 동시성 제어
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/keyword/JpaPopularKeywordRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/keyword/JpaPopularKeywordRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaPopularKeywordRepository extends JpaRepository<PopularKeywordEntity, Long>  {
     Optional<PopularKeywordEntity> findByKeywordAndRegion(String keyword, String region);
@@ -14,4 +16,9 @@ public interface JpaPopularKeywordRepository extends JpaRepository<PopularKeywor
     @Query("SELECT p.keyword, p.count FROM PopularKeywordEntity p" + 
            "WHERE p.region = :region ORDER BY p.count DESC")
     List<Object[]> findTopKeywordByRegion(String region, Pageable pageable);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE PopularKeywordEntity p SET p.count = p.count + 1" + 
+           "WHERE p.keyword = :keyword AND p.region = :region")
+    int updatKeywordCount(@Param("keyword") String keyword, @Param("region") String region);
 }
