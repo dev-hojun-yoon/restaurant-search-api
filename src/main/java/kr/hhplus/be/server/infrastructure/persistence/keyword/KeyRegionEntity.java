7a4b861dbb5e60f0016533870fc313f1644414d1
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:33:56 +0900

    key_region 테이블 엔티티 정의
    key_region 테이블 엔티티 정의
    
    Changelog: changed

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/keyword/KeyRegionEntity.java
@@ -0,0 +1,38 @@
+package kr.hhplus.be.server.infrastructure.persistence.keyword;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "key_region")
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeyRegionEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String regionName;
+
+    public KeyRegionEntity(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    
+}
