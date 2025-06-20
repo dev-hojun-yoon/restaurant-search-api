ed67a7bec90770943fd03f58b60f8b42885cf123
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:12:00 +0900

    인기 키워드 service 보완
    인기 키워드 service 보완
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/application/search/PopularKeywordService.java
+++ src/main/java/kr/hhplus/be/server/application/search/PopularKeywordService.java
@@ -1,19 +1,31 @@
 package kr.hhplus.be.server.application.search;
-
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import kr.hhplus.be.server.domain.keyword.PopularKeyword;
 import kr.hhplus.be.server.domain.keyword.PopularKeywordRepository;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PopularKeywordService {
-    private final PopularKeywordRepository repository;
+    private final PopularKeywordRepository popularKeywordRepository;
+    private final PopularKeywordMemoryService memoryService;
 
     public List<PopularKeyword> findTop(int limit) {
-        return repository.findTopKeywords(limit);
+        return popularKeywordRepository.findTopKeywords(limit);
+    }
+
+    public List<PopularKeyword> findTopByRegion(int limit, String region) {
+        return popularKeywordRepository.findTopKeywordsByRegion(limit, region);
+    }
+
+    public List<String> getAllRegions() {
+        return new ArrayList<>(popularKeywordRepository.getAllRegions());
+    }
+
+    public void increaseCount(String keyword) {
+        memoryService.increaseCount(keyword);
     }
 }
