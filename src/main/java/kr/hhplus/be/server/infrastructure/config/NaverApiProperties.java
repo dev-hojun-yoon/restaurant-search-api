c3d361e4982b9e3e330ef41936d1520d0bc41db9
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:24:54 +0900

    naver api property 보완
    naver api property 보완
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/infrastructure/config/NaverApiProperties.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/config/NaverApiProperties.java
@@ -3,34 +3,34 @@ package kr.hhplus.be.server.infrastructure.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @ConfigurationProperties(prefix = "naver.api")
-public class NaverApiProperties {
+public class NaverApiProperties implements ApiProperties{
     private String baseUrl;
     private String clientId;
     private String clientSecret;
 
+    @Override
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+    public String getClientId() {
+        return clientId;
+    }
+    public String getClientSecret() {
+        return clientSecret;
+    }
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
     }
-
     public void setClientId(String clientId) {
         this.clientId = clientId;
     }
-
     public void setClientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
     }
 
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
+    
+    
 }
