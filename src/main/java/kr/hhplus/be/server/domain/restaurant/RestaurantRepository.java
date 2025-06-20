f9e4823bdadde6fb16d36baaaf841556ed702d22
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:17:51 +0900

    맛집 조회 interface 에서 DB에 적재된 맛집 정보 조회 기능 추가
    맛집 조회 interface 에서 DB에 적재된 맛집 정보 조회 기능 추가
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/domain/restaurant/RestaurantRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/restaurant/RestaurantRepository.java
@@ -4,4 +4,7 @@ import java.util.List;
 
 public interface RestaurantRepository {
     void save(List<Restaurant> restaurants);
+
+    // DB 에서 쿼리로 맛집을 검색하는 메서드 추가
+    List<Restaurant> findByQuery(String query);
 }
