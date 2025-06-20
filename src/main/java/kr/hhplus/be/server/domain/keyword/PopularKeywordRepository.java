216f3701637562901b6a31c064f02671536ef1b9
Hojun Yoon <leid07@aispera.com>
2025-06-20 23:09:17 +0900

    repo increaseCount 기능 복원
    repo increaseCount 기능 복원
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/domain/keyword/PopularKeywordRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/keyword/PopularKeywordRepository.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public interface PopularKeywordRepository {
     void syncKeywordCount(String keyword, String region, Long count);
-    // void increaseCount(String keyword, String region);
+    void increaseCount(String keyword, String region);
     List<PopularKeyword> findTopKeywords(int limit);
     List<PopularKeyword> findTopKeywordsByRegion(int limit, String region);
     List<PopularKeyword> findAllKeywords();
