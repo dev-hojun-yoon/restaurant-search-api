cb59d290effeac310d22f3aaf1e53a143fecc347
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:38:29 +0900

    JPA repo 명시적 지정, 스케줄링 활성화
    JPA repo 명시적 지정, 스케줄링 활성화
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,8 +2,15 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaRepositories(basePackages = {
+	"kr.hhplus.be.server.infrastructure.persistence.keyword",
+	"kr.hhplus.be.server.infrastructure.persistence.restaurant"
+})
 @SpringBootApplication
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
