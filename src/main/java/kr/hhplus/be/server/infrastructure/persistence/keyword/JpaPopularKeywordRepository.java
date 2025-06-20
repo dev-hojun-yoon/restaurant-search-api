55ef40245f08b6366dd3ae34bd880a8997b072b8
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:33:16 +0900

    인기 키워드 조회 기능 보완
    인기 키워드 조회 기능 보완
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/keyword/JpaPopularKeywordRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/keyword/JpaPopularKeywordRepository.java
@@ -4,8 +4,14 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface JpaPopularKeywordRepository extends JpaRepository<PopularKeywordEntity, String>  {
-    Optional<PopularKeywordEntity> findByKeyword(String keyword);
+public interface JpaPopularKeywordRepository extends JpaRepository<PopularKeywordEntity, Long>  {
+    Optional<PopularKeywordEntity> findByKeywordAndRegion(String keyword, String region);
     List<PopularKeywordEntity> findAllByOrderByCountDesc(Pageable pageable);
+
+    // 특정 지역의 인기 키워드 조회.. 위 optional 에서는 부족한지?
+    @Query("SELECT p.keyword, p.count FROM PopularKeywordEntity p" + 
+           "WHERE p.region = :region ORDER BY p.count DESC")
+    List<Object[]> findTopKeywordByRegion(String region, Pageable pageable);
 }
