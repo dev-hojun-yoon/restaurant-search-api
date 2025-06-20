f5a5f4ad8f65a2b540ee5c22775a574e44e0fb7f
Hojun Yoon <leid07@aispera.com>
2025-06-20 21:36:39 +0900

    맛집 조회 repo JPA 정의
    맛집 조회 repo JPA 정의
    
    Changelog: changed

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/JpaRestaurantRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/JpaRestaurantRepository.java
@@ -1,6 +1,10 @@
 package kr.hhplus.be.server.infrastructure.persistence.restaurant;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaRestaurantRepository extends JpaRepository<RestaurantEntity, Long>{    
+    // title 필드에서 query 문자열을 포함하는 결과를 대소문자 구분 없이 검색
+    List<RestaurantEntity> findByTitleContainingIgnoreCase(String query);
 }
