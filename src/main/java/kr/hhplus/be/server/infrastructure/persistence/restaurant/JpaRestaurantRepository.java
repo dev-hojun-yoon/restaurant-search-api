72578ccf571a3562d108a2cac43fde838044281d
Hojun Yoon <leid07@aispera.com>
2025-06-28 04:44:50 +0900

    - 맛집 정보 저장 upsert 쿼리 작성 - 외부 API 오류 발생 시 자체 운영 DB 쿼리 가능하도록 기능 보완

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/JpaRestaurantRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/JpaRestaurantRepository.java
@@ -1,10 +1,41 @@
 package kr.hhplus.be.server.infrastructure.persistence.restaurant;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface JpaRestaurantRepository extends JpaRepository<RestaurantEntity, Long>{    
+public interface JpaRestaurantRepository extends JpaRepository<RestaurantEntity, Long> {    
+    @Modifying
+    @Query(value = """
+            INSERT INTO restaurant (road_address, title, category, mapx, mapy)
+            VALUES (:roadAddress, :title, :category, :mapx, :mapy)
+            ON DUPLICATE KEY UPDATE
+                title = VALUES(title),
+                category = VALUES(category),
+                mapx = VALUES(mapx),
+                mapy = VALUES(mapy)
+            """, nativeQuery = true)
+    void upsertRestaurant(
+        @Param("roadAddress") String roadAddress,
+        @Param("title") String title,
+        @Param("category") String category,
+        @Param("mapx") double mapx,
+        @Param("mapy") double mapy
+    );
+
+    Optional<RestaurantEntity> findByRoadAddress(String roadAddress);
+    
     // title 필드에서 query 문자열을 포함하는 결과를 대소문자 구분 없이 검색
-    List<RestaurantEntity> findByTitleContainingIgnoreCase(String query);
+    // List<RestaurantEntity> findByTitleContainingIgnoreCase(String query);
+
+    @Query(
+        value = "SELECT * FROM restaurant WHERE MATCH(title) AGAINST (:query IN BOOLEAN MODE)",
+        nativeQuery = true
+    )
+    List<RestaurantEntity> searchByFullText(@Param("query") String query);
+
 }
