c10b865167047f939bbe23e76c734064c32c60ee
Hojun Yoon <leid07@aispera.com>
2025-06-28 04:46:28 +0900

    upsert 쿼리 등 적용

--- src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/RestaurantRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/persistence/restaurant/RestaurantRepositoryImpl.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.infrastructure.persistence.restaurant;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
@@ -15,20 +17,49 @@ public class RestaurantRepositoryImpl implements RestaurantRepository{
 
     private final JpaRestaurantRepository jpaRepository;
 
+    // @Override
+    // public void save(List<Restaurant> restaurants) {
+    //     List<RestaurantEntity> entities = restaurants.stream()
+    //         .map(RestaurantEntity::fromDomain)
+    //         .collect(Collectors.toList());
+
+    //     jpaRepository.saveAll(entities);
+    // }
+    // @Override
+    // public void save(List<Restaurant> restaurants) {
+    //     List<RestaurantEntity> upsertEntities = new ArrayList<>();
+
+    //     for (Restaurant restaurant : restaurants) {
+    //         String roadAddress = restaurant.getAddress();
+    //         Optional<RestaurantEntity> existing = jpaRepository.findByRoadAddress(roadAddress);
+
+    //         RestaurantEntity entity = existing
+    //                                     .map(e -> e.updateFromDomain(restaurant))
+    //                                     .orElse(RestaurantEntity.fromDomain(restaurant));
+
+    //         upsertEntities.add(entity);
+    //     }
+
+    //     jpaRepository.saveAll(upsertEntities);
+    // }
     @Override
     public void save(List<Restaurant> restaurants) {
-        List<RestaurantEntity> entities = restaurants.stream()
-            .map(RestaurantEntity::fromDomain)
-            .collect(Collectors.toList());
-
-        jpaRepository.saveAll(entities);
+        for (Restaurant restaurant : restaurants) {
+            jpaRepository.upsertRestaurant(
+                restaurant.getAddress(),
+                restaurant.getTitle(),
+                restaurant.getCategory(),
+                restaurant.getMapx(),
+                restaurant.getMapy()
+            );
+        }
     }
 
     // DB 에서 쿼리로 맛집을 검색하는 기능의 구현부
     @Override
     public List<Restaurant> findByQuery(String query) {
         // 1. JpaRepository 를 호출하여 Entity 리스트를 가져옴
-        List<RestaurantEntity> entities = jpaRepository.findByTitleContainingIgnoreCase(query);
+        List<RestaurantEntity> entities = jpaRepository.searchByFullText(query);
 
         // 2. Entity 리스트를 Domain (DTO) 객체 리스트로 변환
         return entities.stream()
