417187817c7eb2143c1b7553a2c38a92e88a9a09
Hojun Yoon <leid07@aispera.com>
2025-07-25 17:03:42 +0900

    - 틀랜젹션 시 키워드 카운트는 PopularKeywordService 를 거치도록 수정

--- src/main/java/kr/hhplus/be/server/application/search/RestaurantTransactionService.java
+++ src/main/java/kr/hhplus/be/server/application/search/RestaurantTransactionService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.hhplus.be.server.domain.keyword.PopularKeywordRepository;
 import kr.hhplus.be.server.domain.restaurant.Restaurant;
 import kr.hhplus.be.server.domain.restaurant.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +15,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RestaurantTransactionService {
 
-   private final RestaurantRepository repository; 
-   private final PopularKeywordRepository keywordRepository;
-    
+   private final RestaurantRepository repository;
+   private final PopularKeywordService popularKeywordService; // 변경
+
    @Transactional
     public void saveResultsAndUpdateKeywordBlocking(List<Restaurant> restaurants, String query) {
         log.info("DB에 {} 개 결과 저장 및 키워드 카운트 업데이트", restaurants.size());
         repository.save(restaurants);
 
-        String region = keywordRepository.extractRegionFromKeyword(query);
+        String region = popularKeywordService.extractRegionFromKeyword(query);
         log.info("추출된 지역: {}", region);
 
-        keywordRepository.increaseCount(query, region);
-        log.info("DB 저장 완료");
-    } 
+        // PopularKeywordService를 통해 카운트 증가
+        popularKeywordService.increaseCount(query, region);
+        log.info("DB 저장 및 카운트 업데이트 완료");
+    }
 }
